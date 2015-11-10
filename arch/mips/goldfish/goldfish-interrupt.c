24a2e54f21d7b783ff5030f4d1f40fa3cebc8f70
Miodrag Dinic <miodrag.dinic@imgtec.com>
2015-11-10 12:47:54 +0100

    MIPS: goldfish_pic: Add irq domain
    v3.18 kernel requires IRQ domains to be defined for all
    interrupt controlers.
    
    This commit implements irq domain for the Goldfish PIC.
    
    Change-Id: I5875eae22f7eec5a4c13dc094f0a31ea22adc368
    Signed-off-by: Miodrag Dinic <miodrag.dinic@imgtec.com>

--- arch/mips/goldfish/goldfish-interrupt.c
+++ arch/mips/goldfish/goldfish-interrupt.c
@@ -21,6 +21,7 @@
 #include <linux/delay.h>
 #include <linux/irqchip.h>
 #include <linux/of.h>
+#include <linux/of_address.h>
 
 #include <mach/hardware.h>
 #include <mach/irq.h>
@@ -28,24 +29,25 @@
 #include <asm/irq_cpu.h>
 #include <asm/setup.h>
 
-#define GOLDFISH_INTERRUPT_STATUS       0x00 // number of pending interrupts
-#define GOLDFISH_INTERRUPT_NUMBER       0x04
-#define GOLDFISH_INTERRUPT_DISABLE_ALL  0x08
-#define GOLDFISH_INTERRUPT_DISABLE      0x0c
-#define GOLDFISH_INTERRUPT_ENABLE       0x10
+#define GOLDFISH_PIC_STATUS       0x00 // number of pending interrupts
+#define GOLDFISH_PIC_NUMBER       0x04
+#define GOLDFISH_PIC_DISABLE_ALL  0x08
+#define GOLDFISH_PIC_DISABLE      0x0c
+#define GOLDFISH_PIC_ENABLE       0x10
 
-static void __iomem *goldfish_interrupt;
+static struct irq_domain *goldfish_pic_domain;
+static void __iomem *goldfish_pic_base;
 
 void goldfish_mask_irq(struct irq_data *d)
 {
 	writel(d->irq-GOLDFISH_IRQ_BASE,
-	       goldfish_interrupt + GOLDFISH_INTERRUPT_DISABLE);
+	       goldfish_pic_base + GOLDFISH_PIC_DISABLE);
 }
 
 void goldfish_unmask_irq(struct irq_data *d)
 {
 	writel(d->irq-GOLDFISH_IRQ_BASE,
-	       goldfish_interrupt + GOLDFISH_INTERRUPT_ENABLE);
+	       goldfish_pic_base + GOLDFISH_PIC_ENABLE);
 }
 
 static struct irq_chip goldfish_irq_chip = {
@@ -55,45 +57,13 @@ static struct irq_chip goldfish_irq_chip = {
 	.irq_unmask = goldfish_unmask_irq,
 };
 
-void goldfish_init_irq(void)
-{
-	unsigned int i;
-	uint32_t base;
-	struct device_node *dn;
-
-	if ((dn = of_find_node_by_name(NULL, "goldfish_pic")) == NULL) {
-		panic("goldfish_init_irq() failed to "
-			  "fetch device node \'goldfish-pic\'!\n");
-	}
-
-	if (of_property_read_u32(dn, "reg", &base) < 0) {
-		panic("goldfish_init_irq() failed to "
-			  "fetch device base address property \'reg\'!\n");
-	}
-
-	goldfish_interrupt = IO_ADDRESS(base);
-
-	/*
-	 * Disable all interrupt sources
-	 */
-	writel(1, goldfish_interrupt + GOLDFISH_INTERRUPT_DISABLE_ALL);
-
-	for (i = GOLDFISH_IRQ_BASE; i < GOLDFISH_IRQ_BASE+32; i++) {
-		irq_set_chip(i, &goldfish_irq_chip);
-		irq_set_handler(i, handle_level_irq);
-#if 0
-		set_irq_flags(i, IRQF_VALID | IRQF_PROBE);
-#endif
-	}
-}
-
 void goldfish_irq_dispatch(void)
 {
 	uint32_t irq;
 	/*
 	 * Disable all interrupt sources
 	 */
-	irq = readl(goldfish_interrupt + GOLDFISH_INTERRUPT_NUMBER);
+	irq = readl(goldfish_pic_base + GOLDFISH_PIC_NUMBER);
 	do_IRQ(GOLDFISH_IRQ_BASE+irq);
 }
 
@@ -127,20 +97,61 @@ static void mips_timer_dispatch(void)
 	do_IRQ(MIPS_CPU_IRQ_BASE + MIPS_CPU_IRQ_COMPARE);
 }
 
-void __init goldfish_pic_init(struct device_node *node, struct device_node *parent)
+static int goldfish_pic_map(struct irq_domain *d, unsigned int irq, irq_hw_number_t hw)
 {
+	struct irq_chip *chip = &goldfish_irq_chip;
+
+	if (hw < GOLDFISH_IRQ_BASE)
+		return 0;
+
+	irq_set_chip_and_handler(hw, chip, handle_level_irq);
+
+	return 0;
+}
+
+static const struct irq_domain_ops irq_domain_ops = {
+	.xlate = irq_domain_xlate_onetwocell,
+	.map = goldfish_pic_map,
+};
+
+int __init goldfish_pic_of_init(struct device_node *node, struct device_node *parent)
+{
+	struct resource res;
+
+	if (of_address_to_resource(node, 0, &res))
+		panic("Failed to get icu memory range");
+
+	if (request_mem_region(res.start, resource_size(&res),
+				res.name) < 0)
+		pr_err("Failed to request icu memory");
+
+	goldfish_pic_base = ioremap_nocache(res.start, resource_size(&res));
+
+	if (!goldfish_pic_base)
+		panic("Failed to remap icu memory");
+
+	/*
+	 * Disable all interrupt sources
+	 */
+	writel(1, goldfish_pic_base + GOLDFISH_PIC_DISABLE_ALL);
+
 	mips_cpu_irq_init();
-	goldfish_init_irq();
 
 	if (cpu_has_vint) {
+		pr_info("Setting up vectored interrupts\n");
 		set_vi_handler(MIPS_CPU_IRQ_PIC, goldfish_irq_dispatch);
 		set_vi_handler(MIPS_CPU_IRQ_PIC, goldfish_fiq_dispatch);
 	}
+
 	setup_irq(MIPS_CPU_IRQ_BASE+MIPS_CPU_IRQ_PIC, &cascade);
 	setup_irq(MIPS_CPU_IRQ_BASE+MIPS_CPU_IRQ_FIQ, &cascade);
 
 	if (cpu_has_vint)
 		set_vi_handler(MIPS_CPU_IRQ_COMPARE, mips_timer_dispatch);
+
+	goldfish_pic_domain = irq_domain_add_linear(node, NR_IRQS, &irq_domain_ops, 0);
+
+	return 0;
 }
 
 void __init arch_init_irq(void)
@@ -150,4 +161,4 @@ void __init arch_init_irq(void)
 
 static const struct of_device_id irqchip_of_match_goldfish_pic
 __used __section(__irqchip_of_table)
-= { .compatible = "generic,goldfish-pic", .data = goldfish_pic_init };
+= { .compatible = "generic,goldfish-pic", .data = goldfish_pic_of_init };
